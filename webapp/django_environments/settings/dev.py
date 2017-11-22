223d491134aa4a468159d65c9f538e8de0d0c10f
Rodrigo Gil Lopetegui <grodrigo.dev@gmail.com>
2017-11-22 04:28:38 -0300

    django-toolbar fix, added docker default ip to INTERNAL_IPS

--- webapp/django_environments/settings/dev.py
+++ webapp/django_environments/settings/dev.py
@@ -9,15 +9,6 @@ MIDDLEWARE += (
 	'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-INTERNAL_IPS = ('127.0.0.1', )
-
-def show_toolbar(request):
-    if request.is_ajax():
-        return False
-    return True
-
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': 'django_environments.settings.dev.show_toolbar',
-}
+INTERNAL_IPS = ('127.0.0.1', '172.18.0.1', )
 
 
