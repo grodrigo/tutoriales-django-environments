6bb83c30094551527283fd54fed32a2d770490c2
Rodrigo Gil Lopetegui <grodrigo.dev@gmail.com>
2017-11-22 03:41:52 -0300

    Upgraded to django1.11 and postgres9.6. Fixed hidden django-debug-toolbar
    The Debug Toolbar is shown only if your IP is listed in the INTERNAL_IPS setting. Added a callback to solve it on Docker.
    Remember to change settings on production!!!
    
    Upgrades:
    django1.11, postgres9.6, django-debug-toolbar, python3.6, django-debug-toolbar==1.9, django-extensions==1.9, psycopg2==2.7
    
           modified:   base.yml
           modified:   webapp/django_environments/settings/base.py
           modified:   webapp/django_environments/settings/dev.py
           modified:   webapp/django_environments/urls.py
           modified:   webapp/django_environments/wsgi.py
           modified:   webapp/requirements/base.txt
           modified:   webapp/requirements/dev.txt

--- webapp/django_environments/settings/dev.py
+++ webapp/django_environments/settings/dev.py
@@ -5,3 +5,19 @@ INSTALLED_APPS += [
 	'debug_toolbar',
 ] 
 
+MIDDLEWARE += (
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ('127.0.0.1', )
+
+def show_toolbar(request):
+    if request.is_ajax():
+        return False
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'django_environments.settings.dev.show_toolbar',
+}
+
+
--- webapp/django_environments/wsgi.py
+++ webapp/django_environments/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for django_environments project.
+WSGI config for docker_test project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
